ad93c46cbc5199402ea944f1d2d7575e851ac33f
Vivek <sridharvivek88@gmail.com>
2015-09-26 00:02:51 -0400

    Added webservice and logfile

---
+++ App_Code/CreateLogFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+/// <summary>
+/// Summary description for CreateLogFile
+/// </summary>
+public class CreateLogFile
+{
+    
+   private string sLogFormat;
+		private string sErrorTime;
+
+		public CreateLogFile()
+		{
+			//sLogFormat used to create log files format :
+			// dd/mm/yyyy hh:mm:ss AM/PM ==> Log Message
+			sLogFormat = DateTime.Now.ToShortDateString().ToString()+" "+DateTime.Now.ToLongTimeString().ToString()+" ==> ";
+			
+			//this variable used to create log filename format "
+			//for example filename : ErrorLogYYYYMMDD
+			string sYear	= DateTime.Now.Year.ToString();
+			string sMonth	= DateTime.Now.Month.ToString();
+			string sDay	= DateTime.Now.Day.ToString();
+			sErrorTime = sYear+sMonth+sDay;
+		}
+
+		public void ErrorLog(string sPathName, string sErrMsg)
+		{
+			StreamWriter sw = new StreamWriter(sPathName+sErrorTime,true);
+			sw.WriteLine(sLogFormat + sErrMsg);
+			sw.Flush();
+			sw.Close();
+		}
+
+
+    [System.Web.Services.WebMethodAttribute(), System.Web.Script.Services.ScriptMethodAttribute()]
+    public static string GetDynamicContent(string contextKey)
+    {
+        return default(string);
+    }
+
+
+
+}
---
+++ App_Code/WebService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService {
+
+    public WebService () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    
+    [WebMethod]
+    public static string[] GetCompletionList(string prefixText, int count, string contextKey)
+    {
+
+        string[] cmpname_list = {"Tata Consultancy Services (TCS)","Infosys Technologies Ltd","Wipro Limited","Hewlett-Packard India Pvt. Ltd","IBM India Limited",
+"Satyam Computer Services Ltd.","HCL Technologies Ltd.","Intel Technology India Pvt. Ltd.","CISCO Systems India Pvt. Ltd.",
+"Patni Computer Systems Ltd.","Cognizant Technology Solutions India","HCL Infosystems Ltd.","Mahindra-British Telecom",
+"Redington (India) Limited","i-Flex Solutions Ltd.","Oracle India Pvt. Ltd.","Moser Baer India Ltd.","Microsoft Corporation (India) Pvt. Ltd.",
+"NIIT Limited","TATA Infotech Ltd.","iGATE Global Solutions Ltd.","Ingram Micro India (P) Ltd","Sun Microsystems India Pvt Ltd",
+"Polaris Software Lab Ltd.","Samsung India Electronics Ltd.","Perot Systems TSI (India) Limited","SAP India Pvt. Ltd.","American Power Conversion",
+"Celetronix India Pvt. Ltd.","Computer Associates India Pvt. Ltd","Dell Computer India Pvt. Ltd.","Hexaware Technologies","Larsen & Toubro Infotech Ltd.",
+"Siemens Information Systems Ltd.","Mastek Ltd.","CMS Computers Ltd.","Texas Instruments India Pvt. Ltd","Honeywell Technology Solutions Lab",
+"Acer India (Pvt) Ltd.","MphasiS BFL Ltd.","Datacraft India Limited","Nortel Networks (I) Pvt Ltd","Syntel (India) Ltd.",
+"Flextronics Software Systems Ltd","Kanbay Software India Pvt. Ltd.","Rolta India Ltd.","Infinite Computer Solutions (I) Pvt Ltd",
+"GTL Limited","Covansys (I) Pvt. Ltd.","Sify Limited","Zensar Technologies Ltd.","Tulip IT Services Ltd","Zenith Computers Limited",
+"Sonata Software Ltd","D-Link (India) Ltd.","Mascon Global Limited","3i Infotech Limited","WeP Peripherals Ltd.","Computer Sciences Corporation India",
+"Philips Innovation Campus","Xansa India","Canon India Pvt. Ltd.","TVS Electronics Ltd.","Infotech Enterprises Ltd.","KPIT Cummins Infosystems Limited",
+"ITC Infotech India","MindTree Consulting Pvt. Ltd.","PCS Technology Ltd.","Sasken Communication Technologies","Tally Solutions Pvt. Ltd.",
+"Ramco Systems Ltd.","Epson India Pvt. Ltd.","Numeric Power Systems Ltd.","Rashi Peripherals Pvt. Ltd","Network Solutions Pvt. Ltd.",
+"TATA Elxsi Ltd.","Aftek Infosys Limited","EMC Data Storage Systems India","Tata Technologies Ltd.","Geometric Software Solutions Co.",
+"Cranes Software International Limited","Accel Frontline Limited","Mediaman Infotech Pvt. Ltd.","Priya Limited - IT Products Division",
+"Keane India Ltd.","Persistent Systems Pvt. Ltd.","Aptech Limited","Lipi Data Systems Ltd.","Cadence Design Systems (India)",
+"Intex Technologies (India) Ltd","Aditi Technologies Pvt Ltd","MICROTEK Limited","Subex Systems Limited","Blue Star Infotech Limited",
+"Aztec Software & Technology Services","Pentamedia Graphics Ltd.","TATA Interactive Systems","Birlasoft Limited","DB Power Electronics (P) Ltd",
+"Atlanta IT Solutions Pvt. Ltd","RMSI Pvt. Ltd.",
+};
+        // Return matching Company Name
+        return (from m in cmpname_list where m.StartsWith(prefixText, StringComparison.CurrentCultureIgnoreCase) select m).Take(count).ToArray();
+    }
+}
